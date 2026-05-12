671237a80a2ebc472d26bd339a15435266028344
Jyothi-Swaroop5 <pjyothiswaroop129@gmail.com>
2026-05-12 15:49:57 +0530

    added Data_Types.py file

---
+++ Week1/Data_Types.py
@@ -0,0 +1,9 @@
+integer_num = 10      
+decimal_num = 3.14     
+text = "Hello"          
+is_active = True       
+
+print(type(integer_num))
+print(type(decimal_num))
+print(type(text))
+print(type(is_active))
